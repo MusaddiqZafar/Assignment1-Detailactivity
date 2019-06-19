3c4f62c43a42666b73aa11c6023fceac59aad304
Musaddiq <>
2019-06-19 10:26:27 +0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment1detailactivity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.assignment1detailactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+
+        Toast.makeText(this, "djkaskldajsklads", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+}
